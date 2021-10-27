f21820a7b7ede76974eb5cf88b06532401ae4b91
Ciibelle <85749588+Ciibelle@users.noreply.github.com>
2021-10-27 17:28:50 -0300

    Add files via upload

---
+++ Tarefa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tarefa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade=0;
+            int contador=1;
+            int qtdPessoas=0;
+
+           while (contador <= 10)
+           {
+               contador++;
+               Console.WriteLine("Informe sua idade!");
+               idade = Convert.ToInt32(Console.ReadLine());
+           
+               if (idade >= 18)
+           {
+               qtdPessoas = qtdPessoas + 1;
+           }
+
+           }
+           Console.WriteLine($"{qtdPessoas} pessoas têm permissão para dirigir!");
+        }
+    }
+}
